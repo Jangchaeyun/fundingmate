b074f59d280d85939dcb4c8ba5c83b8e1b506142
Jangchaeyun <zzangsally@naver.com>
2023-06-27 10:51:23 +0900

    fundingmate

---
+++ fundingmate/src/test/java/com/fund/fundingmate/FundingmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fund.fundingmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundingmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
