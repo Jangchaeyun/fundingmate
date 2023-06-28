c241e6aae3374a77e0b7dd42c6c20c206770ec1c
Jangchaeyun <zzangsally@naver.com>
2023-06-28 14:17:22 +0900

    App

--- fundingmate-front/src/App.js
+++ fundingmate-front/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Reward from "./Component/Reward/Reward";
+import Rewarddetail from "./Component/RewardDetail/Rewarddetail";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         {/* <Route exact path="/" element={<Main />} /> */}
         <Route exact path="/reward" element={<Reward />} />
+        <Route exact path="/reward-detail" element={<Rewarddetail />} />
       </Routes>
     </div>
   );
