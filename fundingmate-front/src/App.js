f3b0dbd452e5636ea5fdacb9d72252ed2da67bd1
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2023-06-28 19:59:52 +0900

    App.js

--- fundingmate-front/src/App.js
+++ fundingmate-front/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<Main />} />
+        {/* <Route exact path="/" element={<Main />} /> */}
         <Route exact path="/reward" element={<Reward />} />
         <Route exact path="/reward-detail" element={<Rewarddetail />} />
       </Routes>
