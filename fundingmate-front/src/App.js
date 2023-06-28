c15c85543eca7ad095bd6a5796b6a750bd6ccfb5
zzangsally <62280276+Jangchaeyun@users.noreply.github.com>
2023-06-28 19:48:07 +0900

    app.js

--- fundingmate-front/src/App.js
+++ fundingmate-front/src/App.js
@@ -1,5 +1,9 @@
 import { Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import "./App.css";
+import Reward from "./pages/Reward/Reward";
+import Rewarddetail from "./pages/Rewarddetail/Rewarddetail";
+import Home from "./pages/Home/Home";
 import Reward from "./pages/Reward/Reward";
 import Rewarddetail from "./pages/Rewarddetail/Rewarddetail";
 import Home from "./pages/Home/Home";
@@ -7,13 +11,11 @@ import Home from "./pages/Home/Home";
 function App() {
   return (
     <div className="App">
-      {/* <Home />
-      //{" "} */}
+      <Home />
       <Routes>
-        // {/* <Route exact path="/" element={<Main />} /> */}
-        // <Route exact path="/reward" element={<Reward />} />
-        // <Route exact path="/reward-detail" element={<Rewarddetail />} />
-        //{" "}
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/reward" element={<Reward />} />
+        <Route exact path="/reward-detail" element={<Rewarddetail />} />
       </Routes>
     </div>
   );
