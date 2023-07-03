932b814187b60d4766aa1dee339fb4897fe07cda
Becca <123378408+beccajoe@users.noreply.github.com>
2023-07-03 09:41:56 +0900

    Updated mapping for My Page (Founder)

--- fundingmate-front/src/App.js
+++ fundingmate-front/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Reward from "./Component/Reward/Reward";
+import Founder from "./pages/MyPage/Founder";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         {/* <Route exact path="/" element={<Main />} /> */}
         <Route exact path="/reward" element={<Reward />} />
+        <Route exact path="/my-page?type=founder" element={<Founder />} />
       </Routes>
     </div>
   );
