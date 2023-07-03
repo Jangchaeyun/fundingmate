5b123a65d97a9c8e2137bde17b7523b6b94f9581
yth <f_hyang@naver.com>
2023-07-04 01:28:52 +0900

    2023 07 04 1:28 makeReward

--- fundingmate-front/src/Component/MakeReward/MakeReward2.js
+++ fundingmate-front/src/Component/MakeReward/MakeReward2.js
@@ -2,8 +2,8 @@ import React, { useRef, useState } from 'react';
 import "./MakeReward2.css";
 import "./MakeRewardCommon.css";
 
+import {PlusCircleOutlined, PlusSquareOutlined, MinusCircleOutlined, MinusSquareOutlined} from "@ant-design/icons";
 
-import {PlusCircleOutlined, PlusSquareOutlined} from "@ant-design/icons";
 
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
@@ -11,32 +11,85 @@ import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 import 'tui-color-picker/dist/tui-color-picker.css';
 import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
 import '@toast-ui/editor/dist/i18n/ko-kr';
-
+const MAX_IMAGES = 15;
 
 const MakeReward2 = () => {
     const editorRef = useRef();
     const onChange = () => {
         const data = editorRef.current.getInstance().getHTML();
         console.log(data);}
-    const [selectedImages, setSelectedImages] = useState([]);
 
+    const [inputs, setInputs] = useState([{ id: 1 }]);
+
+    const handleAddInput = () => {
+        const newId = inputs.length + 1;
+        const newInput = { id: newId };
+        setInputs((prevInputs) => [...prevInputs, newInput]);
+    };
+
+    const handleDeleteInput = (id) => {
+        setInputs((prevInputs) => prevInputs.filter((input) => input.id !== id));
+    };
+    const [showDeleteIcon, setShowDeleteIcon] = useState(false);
+    const [images, setImages] = useState([]);
+    const [showDeleteButton, setShowDeleteButton] = useState(false);
     const handleImageUpload = (event) => {
         const files = event.target.files;
+        const newImages = [];
 
-        if (files) {
-            const reader = new FileReader();
+        const uploadImage = (file) => {
+            return new Promise((resolve, reject) => {
+                const reader = new FileReader();
 
-            reader.onload = (e) => {
-                const image = e.target.result;
-                setSelectedImages((prevImages) => [...prevImages, image]);
-            };
+                reader.onload = (e) => {
+                    const image = {
+                        src: e.target.result,
+                        alt: 'Selected',
+                        style: { width: '100%', height: '100%', objectFit: 'cover' }
+                    };
+                    resolve(image);
+                };
 
-            Array.from(files).forEach((file) => reader.readAsDataURL(file));
-        }
+                reader.onerror = (error) => {
+                    reject(error);
+                };
+
+                reader.readAsDataURL(file);
+            });
+        };
+
+        const uploadAllImages = async () => {
+            for (const file of files) {
+                try {
+                    const image = await uploadImage(file);
+                    newImages.push(image);
+                } catch (error) {
+                    console.error('Error uploading image:', error);
+                }
+            }
+            setImages((prevImages) => [...prevImages, ...newImages]);
+        };
+
+        uploadAllImages();
     };
 
     const handleImageClick = () => {
-        document.getElementById('imageUpload').click();
+        if (images.length < MAX_IMAGES) { // Replace MAX_IMAGES with the maximum number of images allowed
+            document.getElementById('imageUpload').click();
+        }
+    };
+
+    const handleImageDelete = (index) => {
+        setImages((prevImages) => {
+            const updatedImages = [...prevImages];
+            updatedImages.splice(index, 1);
+            return updatedImages;
+        });
+        setShowDeleteIcon(false);
+    };
+
+    const handleAddButtonClick = () => {
+        setShowDeleteButton(true);
     };
     return (
         <>
@@ -62,38 +115,62 @@ const MakeReward2 = () => {
             <p className="custom-font-text" style={{fontSize:"12px", marginTop:"11px"}}>
                 <b>동영상 주소를 적어주세요</b>
             </p>
-            <div className="projMake-video">
-            <input type="text" name="rewardVideoAddress" className="input-box"/>
-            <button className="rew-add"><PlusSquareOutlined style={{ fontSize: "23px"}} /></button>
+            <div className="projMake-video" style={{flexDirection:"column", alignItems: "flex-start"}}>
+                {inputs.map((input, index) => (
+                    <div key={input.id} style={{ marginTop: index > 0 ? '10px' : '0' }}>
+                        <input type="text" name="rewardVideoAddress" className="input-box-video"/>
+                        <button className="rew-add" onClick={handleAddInput}>
+                            <PlusSquareOutlined style={{ fontSize: "23px" }} />
+                        </button>
+                        {input.id !== 1 && (
+                            <button className="rew-delete" onClick={() => handleDeleteInput(input.id)}>
+                                <MinusSquareOutlined style={{ fontSize: "23px" }} />
+                            </button>
+                        )}
+                    </div>
+                ))}
             </div>
-            <br/>
+            <br />
 
             <p className="custom-font-text" style={{fontSize:"12px", marginBottom:"11px"}}>
                 <b>이미지를 등록해주세요</b>
             </p>
-            <div className="imi-image">
-                <div className="imi-image-upload">
-                    <div className="imi-image-upload-info" onClick={handleImageClick} >
+
+            <div className="imi-image" style={{ display: 'flex', flexWrap: 'wrap' }}>
+                {images.map((image, index) => (
+                    <div className="imi-image-upload" key={index} style={{ marginTop: '15px' }}>
+                        <div
+                            className="imi-image-upload-info"
+                            onClick={handleImageClick}
+                            onMouseEnter={() => setShowDeleteIcon(true)}
+                            onMouseLeave={() => setShowDeleteIcon(false)}
+                            style={{ cursor: 'pointer', width: '130px', height: '85px' }}
+                        >
+                            <div
+                                className="imi-image-delete"
+                                onClick={() => handleImageDelete(index)}
+                                style={{ position: 'absolute', top: '5px', right: '5px', zIndex: '1', color: '#fff', fontSize: '15px' , display: showDeleteIcon ? 'block' : 'none' }}
+                            >
+                                <MinusCircleOutlined id="imi-image-delete-icon"/>
+                            </div>
+                            <img src={image.src} alt={image.alt} style={image.style} />
+                        </div>
+                    </div>
+                ))}
+                {images.length < MAX_IMAGES - 1 && (
+                <div className="imi-image-upload" style={{ marginTop: '15px' }}>
+                    <div
+                        className="imi-image-upload-info"
+                        onClick={handleImageClick}
+                        style={{ cursor: 'pointer', width: '130px', height: '85px' }}
+                    >
                         <div style={{ marginBottom: '2%' }}>
                             <PlusCircleOutlined style={{ fontSize: '15px', cursor: 'pointer', marginRight: '3px' }} />
                         </div>
                         최적 사이즈<br />740*417px
                     </div>
-
-                    {selectedImages[0] && (
-                        <img src={selectedImages[0]} alt="uploaded" className="uploaded-image" />
-                    )}
                 </div>
-                <div className="imi-image-upload">
-                    <div className="imi-image-upload-info" onClick={handleImageClick}>
-                        <div>
-                            <PlusCircleOutlined style={{ fontSize: '19px', cursor: 'pointer', marginRight: '3px' }} />
-                        </div>
-                    </div>
-                    {selectedImages[1] && (
-                        <img src={selectedImages[1]} alt="uploaded" className="uploaded-image" />
-                    )}
-                </div>
+                )}
             </div>
             <input
                 type="file"
@@ -101,7 +178,6 @@ const MakeReward2 = () => {
                 accept="image/*"
                 style={{ display: 'none' }}
                 onChange={handleImageUpload}
-                multiple
             />
             <br/>
             <br/>
