fd7be1bcbf59bd49e1fedd7bd341c505b8011e28
yountaehee <f_hyang@naver.com>
2023-07-12 17:47:58 +0900

    2023 07 12 17:47 backend

--- fundingmate/src/main/java/com/fund/fundingmate/domain/investment/service/InvestmentServiceImpl.java
+++ fundingmate/src/main/java/com/fund/fundingmate/domain/investment/service/InvestmentServiceImpl.java
@@ -162,59 +162,5 @@ public class InvestmentServiceImpl implements InvestmentService {
     }
 
 
-    /*@Override
-    public void createInvestment(InvestmentDTO investmentDto, Long userId) {
-        Investment investment = new Investment();
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("User not found with ID: " + userId));
 
-        investment.setUser(user);
-        // Set investment properties from investmentDto
-        investment.setInvestNo(investmentDto.getInvestNo());
-        investment.setInvestCategory(investmentDto.getInvestCategory());
-        investment.setInvestProjName(investmentDto.getInvestProjName());
-        investment.setInvestTargetAmount(investmentDto.getInvestTargetAmount());
-        investment.setInvestProjDateStart(investmentDto.getInvestProjDateStart());
-        investment.setInvestProjDateEnd(investmentDto.getInvestProjDateEnd());
-        investment.setInvestRepImgSavedName(investmentDto.getInvestRepImgSavedName());
-        investment.setInvestProjKeyword(investmentDto.getInvestProjKeyword());
-        investment.setUseOfFunds(investmentDto.getUseOfFunds());
-        investment.setUseOfFundsDateStart(investmentDto.getUseOfFundsDateStart());
-        investment.setUseOfFundsDateEnd(investmentDto.getUseOfFundsDateEnd());
-        investment.setRateOfReturn(investmentDto.getRateOfReturn());
-        investment.setExpectedPaymentDate(investmentDto.getExpectedPaymentDate());
-        investment.setRepaymentMethod(investmentDto.getRepaymentMethod());
-        investment.setInvestVideoUrl(investmentDto.getInvestVideoUrl());
-        investment.setInvestContentImgSavedName(investmentDto.getInvestContentImgSavedName());
-        investment.setInvestItemIntro(investmentDto.getInvestItemIntro());
-        investment.setInvestItemBusinessValue(investmentDto.getInvestItemBusinessValue());
-        investment.setInvestItemValue(investmentDto.getInvestItemValue());
-        investment.setInvestItemBenefit(investmentDto.getInvestItemBenefit());
-        investment.setInvestProjContent(investmentDto.getInvestProjContent());
-        investment.setInvestIdBusinessLicenseImgSavedName(investmentDto.getInvestIdBusinessLicenseImgSavedName());
-        investment.setBusinessAddress(investmentDto.getBusinessAddress());
-        investment.setInvestEmail(investmentDto.getInvestEmail());
-        investment.setBank(investmentDto.getBank());
-        investment.setAccNumber(investmentDto.getAccNumber());
-        investment.setDepositorName(investmentDto.getDepositorName());
-        investment.setInvestBankAccountCopyImgSavedName(investmentDto.getInvestBankAccountCopyImgSavedName());
-        investment.setTaxBillEmail(investmentDto.getTaxBillEmail());
-        investment.setWebsiteUrl(investmentDto.getWebsiteUrl());
-        investment.setFacebookUrl(investmentDto.getFacebookUrl());
-        investment.setInstagramUrl(investmentDto.getInstagramUrl());
-        investment.setBlogUrl(investmentDto.getBlogUrl());
-        investment.setTwitterUrl(investmentDto.getTwitterUrl());
-
-        // Set user reference
-       *//* User user = userRepository.findByUserNo(userNo);
-        if (user == null) {
-            // Handle user not found error
-            throw new RuntimeException("User not found");
-        }
-        investment.setUser(user);*//*
-
-
-        // Save the investment in the database
-        investmentRepository.save(investment);
-    }*/
 }
