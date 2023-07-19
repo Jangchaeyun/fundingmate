3c5b2cdf468d6e5444eeeba78a6cbb7204a14c7c
Jangchaeyun <zzangsally@naver.com>
2023-07-19 11:31:12 +0900

    0719

--- fundingmate/src/main/java/com/fund/fundingmate/domain/investment/repository/InvestmentRepository.java
+++ fundingmate/src/main/java/com/fund/fundingmate/domain/investment/repository/InvestmentRepository.java
@@ -3,5 +3,5 @@ package com.fund.fundingmate.domain.investment.repository;
 import com.fund.fundingmate.domain.investment.entity.Investment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface investmentRepository extends JpaRepository<Investment, Long> {
+public interface InvestmentRepository extends JpaRepository<Investment, Long> {
 }
