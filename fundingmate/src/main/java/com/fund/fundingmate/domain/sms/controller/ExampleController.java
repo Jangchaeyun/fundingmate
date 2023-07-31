92c1c255928b9ef44f9e82f56ac1044c08fd0e7a
kec0486 <84944294+kec0486@users.noreply.github.com>
2023-08-01 01:31:12 +0900

    Update ExampleController.java

--- fundingmate/src/main/java/com/fund/fundingmate/domain/sms/controller/ExampleController.java
+++ fundingmate/src/main/java/com/fund/fundingmate/domain/sms/controller/ExampleController.java
@@ -109,7 +109,7 @@ public class ExampleController {
             String cerNum = buffer.toString();
             Message message = new Message();
             // 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
-            message.setFrom("01054439777");
+            message.setFrom("");
             message.setTo(tel);
             message.setText("[펀딩메이트] 문자 본인인증 서비스 : 인증번호는 " + "[" + cerNum + "]" + " 입니다.");
 
